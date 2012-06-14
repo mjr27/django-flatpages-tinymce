fc95a2caa6419f898873811e2e0c0036b91941fc
Santtu Pajukanta <santtu.pajukanta@tut.fi>
2012-06-14 15:42:27 +0000

    templatetags: control adminship via has_perm(flatpages.change_flatpage) instead of is_superuser

--- flatpages_tinymce/templatetags/flatpage_admin.py
+++ flatpages_tinymce/templatetags/flatpage_admin.py
@@ -13,13 +13,12 @@ from django.utils.translation import ugettext as _
 
 register = template.Library()
 
+def is_admin(context):
+    return 'user' in context and context['user'].has_perm('flatpages.change_flatpage') and local_settings.USE_FRONTED_TINYMCE
 
 @register.simple_tag(takes_context=True)
 def flatpage_media(context):
-    is_admin = ('user' in context
-                and context['user'].is_superuser
-                and local_settings.USE_FRONTED_TINYMCE)
-    if not is_admin:
+    if not is_admin(context):
         return ""
 
     if context.render_context.get("flatpage_media_loaded", None):
@@ -70,13 +69,12 @@ def flatpage_media(context):
 
 @register.inclusion_tag("flatpages_tinymce/_contentedit.htm", takes_context=True)
 def flatpage_admin(context, flatpage):
-    is_admin = 'user' in context and context['user'].is_superuser and local_settings.USE_FRONTED_TINYMCE
     media = flatpage_media(context)
     output_content = mark_safe(flatpage.content)
     return {
         'media': media,
         'page_id': flatpage.id,
-        'admin': is_admin,
+        'admin': is_admin(context),
         'prefix': local_settings.DIV_PREFIX,
         'content': output_content,
         }
